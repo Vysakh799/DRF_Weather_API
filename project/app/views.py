7d6f190d25d7491524e20570260004efbcb6003e
Vysakh799 <Vysakh7995@gmail.com>
2024-06-15 09:06:34 +0530

    api adding

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-
+import requests
+import math
+import datetime
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    city_name='Calicut'
+    url=f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=360e4bc3865e745ec844bd7ec054ca11'
+    data=requests.get(url)
+    weather_data=data.json()
+    dt_object=datetime.datetime.fromtimestamp(weather_data['sys']['sunrise'])
+    dt_object1=datetime.datetime.fromtimestamp(weather_data['sys']['sunset'])
+    data={
+        'city':city_name,
+        'weather_discription':weather_data['weather'][0]['description'],
+        'temperature_kelvin':weather_data['main']['temp'],
+        'temperature_celsius':math.floor(weather_data['main']['temp']- 273.15), #Convert to Celsius
+        'humidity':weather_data['main']['humidity'],
+        'temp_min_celsius':math.floor(weather_data['main']['temp_min']- 273.15), #Convert to Celsius
+        'temp_max_celsius':math.floor(weather_data['main']['temp_max']- 273.15), #Convert to Celsius
+        'sunrise':dt_object.strftime('%H:%M:%S'),'sunset':dt_object1.strftime('%H:%M:%S'),
+        'visibility':math.floor(weather_data['visibility']/1000),
+        'cloud':weather_data['clouds']['all'],
+        'wind':weather_data['wind']['speed'],
+        'pressure':math.floor((weather_data['main']['pressure']/1013.25)*100),
+        'feel_temp':math.floor(weather_data['main']['feels_like']-273.15),
+    }
+    return render(request,'index.html',{'data':data})
